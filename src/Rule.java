0f56da635a2a2ff3da2610fcee831b96c616df91
Charlie441324219 <dian.li@ttu.edu>
2017-10-23 18:37:43 -0500

    implement class rule

---
+++ src/Rule.java
@@ -0,0 +1,26 @@
+
+public class Rule {
+    public String rule;
+    public int[] priority;
+
+    public Rule(String rule, int[] priority) {
+        this.rule = rule;
+        this.priority = priority;
+    }
+
+    public String getRule() {
+        return rule;
+    }
+
+    public void setRule(String rule) {
+        this.rule = rule;
+    }
+
+    public int[] getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int[] priority) {
+        this.priority = priority;
+    }
+}
