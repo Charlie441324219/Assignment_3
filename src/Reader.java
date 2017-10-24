eefa89e5fb40cbd9f58fb6d62e5fbe5d02c00f4c
Charlie441324219 <dian.li@ttu.edu>
2017-10-24 16:28:43 -0500

    finish read specific line

--- src/Reader.java
+++ src/Reader.java
@@ -1,20 +1,15 @@
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//
-//public class Reader {
-//
-//    public void readLine(int line) throws IOException {
-//
-//        String fileName = "input.txt";
-//
-//        FileReader fileReader = new FileReader(fileName);
-//        BufferedReader bufferedReader = new BufferedReader(fileReader);
-//
-//        if(int i = 0; i < line; i++){
-//            System.out.print(line);
-//        }
-//
-//    }
-//}
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Reader {
+
+    public void readLine(int line) throws IOException {
+        String lineRead = Files.readAllLines(Paths.get("input.txt")).get(line - 1);
+        System.out.println(lineRead);
+
+    }
+}
