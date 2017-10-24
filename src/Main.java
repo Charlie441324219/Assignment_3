b1e5372c71c1cabf18cf5a2bff45cac4ae140623
Charlie441324219 <dian.li@ttu.edu>
2017-10-24 01:30:37 -0500

    working on txt reader

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 import java.io.IOException;
 
 public class Main {
+
+    final static private int ROW = 300;
+
     public static void main(String[] args) throws IOException {
+
         Generator input = new Generator();
-        input.generateTXT(300);
+        input.generateTXT(ROW);
+
+        Reader reader = new Reader();
+        reader.readLine(1);
     }
+
 }
